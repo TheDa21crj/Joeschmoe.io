e09759de8b990704287007239477fac7800c3653
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-01-08 14:59:15 +0530

    empty bug fixed

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.getElementById("submitBtn").addEventListener("click", function(e) {
     const name = document.getElementById("nameid").value;
-    document
-        .getElementById("newImg")
-        .setAttribute("src", `https://joeschmoe.io/api/v1/${name}`);
+    if (name) {
+        document
+            .getElementById("newImg")
+            .setAttribute("src", `https://joeschmoe.io/api/v1/${name}`);
+    }
 });
