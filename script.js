f6e3a6efedac848b9bb7a1c89484ca32d29cc50f
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-01-08 17:00:52 +0530

    alert Added

--- script.js
+++ script.js
@@ -8,5 +8,16 @@ function shortcut() {
         document
             .getElementById("newImg")
             .setAttribute("src", `https://joeschmoe.io/api/v1/${name}`);
+        document.getElementById("newImg").style.display = "flex";
+        document.getElementById("svgTag").style.display = "none";
+    } else {
+        setTimeout(() => {
+            document.getElementById("alert").style.opacity = "1";
+            document.getElementById("alert").style.visibility = "visible";
+            setTimeout(() => {
+                document.getElementById("alert").style.opacity = "0";
+                document.getElementById("alert").style.visibility = "hidden";
+            }, 1000);
+        });
     }
 }
