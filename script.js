e3f81a6ac034635390ff5f6733fdfb939eb4b905
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-01-08 15:28:28 +0530

    default img updated

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 document.getElementById("submitBtn").addEventListener("click", function(e) {
+    shortcut();
+});
+
+function shortcut() {
     const name = document.getElementById("nameid").value;
     if (name) {
         document
             .getElementById("newImg")
             .setAttribute("src", `https://joeschmoe.io/api/v1/${name}`);
     }
-});
+}
